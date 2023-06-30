94f790a9d557695cb7542a9723b1a723773662d5
Brayan <smithrivas.dev@gmail.com>
2023-06-30 11:18:27 -0500

    Proyecto base

---
+++ views/layout.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="shortcut icon" href="favicon.ico" type="../src/icon.png">
+    <title>El gran bigote</title>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;700;900&display=swap" rel="stylesheet"> 
+    <link rel="stylesheet" href="build/css/app.css">
+</head>
+<body>
+
+    <?php echo $contenido; ?>
+            
+</body>
+</html>
